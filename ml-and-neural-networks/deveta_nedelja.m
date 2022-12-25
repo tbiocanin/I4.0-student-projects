41676426ed69d914705f70fd2a3bd499a6e8b4df
Teodor Biocanin <tekikralj011@gmail.com>
2022-12-25 20:00:59 +0100

    function prob resolved, number recognition added

--- ml-and-neural-networks/deveta_nedelja.m
+++ ml-and-neural-networks/deveta_nedelja.m
@@ -3,7 +3,7 @@ x_pocetak = [0; 0; 0];
 %[x_izlaz, y_izlaz, teta_izlaz] = simulacija_kretanja_kruzno(x_pocetak)
 
 %test 
-[x_izlaz, y_izlaz, teta_izlaz, delta_s_levo, delta_s_desno] = simulacija_kretanje_funkcija(x_pocetak)
+[x_izlaz, y_izlaz, teta_izlaz, delta_s_levo, delta_s_desno] = simulacija_kretanje_funkcija(x_pocetak);
 
 %verifikacija funkcije
 x_test = 0:1000;
