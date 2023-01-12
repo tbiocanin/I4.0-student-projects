40cd24db066c131c23c586cac155075c9294348e
Teodor Biocanin <tekikralj011@gmail.com>
2023-01-12 09:17:02 +0100

    small functional adaptation

--- ml-and-neural-networks/simulacija_kretanje_funkcija.m
+++ ml-and-neural-networks/simulacija_kretanje_funkcija.m
@@ -1,35 +1,37 @@
 function [x_translatorno, y_translatorno, teta_izlaz, delta_s_levo_izlaz, delta_s_desno_izlaz] = simulacija_kretanje_funkcija(x_pocetno)
 
-    %b = 200;
-    %delta_s_levo = 10 + randn(1, 100) %debagovati ovaj deo jer samo mogu translatornu trajektoriju da dobijem ovde, dakle nalik kretanju za slovo. Proveri formulu
-    %delta_s_desno = 10 + randn(1, 100)
-    
-    b = 400;
-    R = 100;
-    alfa = -2*pi/180;
-    delta_s_levo = ones(1, 10) .* (R - b/2)*alfa + randn(1, 10);
-    delta_s_desno = ones(1, 10) .* (R + b/2)*alfa + randn(1, 10);
+    b = 20;  
+    xu = -10:0.1:10; 
+    yu(1) = 2*cos(xu(1)) - cos(xu(1)/2 + pi/2)  + 10;
+    tetau(1) = 0; 
 
-    delta_s = (delta_s_levo + delta_s_desno)/2;
-    x_novo = zeros(3, 10);
-    x_novo(1:3, 1) = x_pocetno;
-
-
-    for i = 2:10
-        x_novo(:, i) = x_novo(:, i-1) + [delta_s(1, i) * cos( x_novo(3, i-1) + ( delta_s_desno(1, i) - delta_s_levo(1, i) )/( 2 * b ) );
-                              delta_s(1, i) * sin( x_novo(3, i-1) + (delta_s_desno(1, i) - delta_s_levo(1, i))/( 2 * b ) );
-                              (delta_s_desno(1, i) - delta_s_levo(1, i))/b];
-        x_novo(2, i) = 2*cos(x_novo(2, i)) + cos(x_novo(2, i)/2 + pi/2) + 10;
-        
+    Dtetau = 0; 
+    for i = 2:200 
+        yu(i) = 2*cos(xu(i)) - cos(xu(i)/2 + pi/2)  + 10; 
+        tetau(i) = atand((yu(i)-yu(i-1))/(xu(i)-xu(i-1))); 
+        su(i) = sqrt((xu(i)-xu(i-1))^2 + (yu(i)-yu(i-1))^2); 
+        Dtetau(i) = tetau(i) - tetau(i-1); 
     end
 
-    x_translatorno = x_novo(1, :);
-    y_translatorno = x_novo(2, :);
-    teta_izlaz = x_novo(3, :);
-    delta_s_levo_izlaz = delta_s_levo;
-    delta_s_desno_izlaz = delta_s_desno;
-    plot(x_translatorno, y_translatorno)
-    %axis equal
-end
+    x(1) = xu(1); 
+    y(1) = yu(1); 
+    teta(1) = tetau(1); 
+    sd(1) = su(1) + 0.5*b*Dtetau(1);
+    sl(1) = su(1) - 0.5*b*Dtetau(1); 
+    izlaz(1:5,1) = [x(1);y(1);teta(1);sd(1);sl(1)]; 
 
-% randn()
+    for i = 2:200 
+        sd(i) = su(i) + 0.5*b*Dtetau(i) + randn*0.05; 
+        sl(i) = su(i) - 0.5*b*Dtetau(i) + randn*0.05; 
+        x(i) = x(i-1) + 0.5*(sd(i)+sl(i))*cosd(teta(i-1) + (sd(i)-sl(i))/(2*b)); 
+        y(i) = y(i-1) + 0.5*(sd(i)+sl(i))*sind(teta(i-1) + (sd(i)-sl(i))/(2*b)); 
+        teta(i) = teta(i-1) + (sd(i)-sl(i))/b; 
+        izlaz(:,i) = [x(i);y(i);teta(i);sd(i);sl(i)]; 
+    end
+
+    x_translatorno = izlaz(1, :);
+    y_translatorno = izlaz(2, :);
+    teta_izlaz = izlaz(3, :);
+    delta_s_desno_izlaz = izlaz(4, :);
+    delta_s_levo_izlaz = izlaz(5, :);
+end
