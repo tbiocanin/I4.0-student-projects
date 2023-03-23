5eacce1287bbb54c3bf6fed821c794111655c217
Teodor Biocanin <tekikralj011@gmail.com>
2023-03-23 22:52:02 +0100

    still going back and forth between wsl2 and matlab

---
+++ ml-and-neural-networks/src/robotika2/testF.m
@@ -0,0 +1,5 @@
+%test function just for idea verification
+
+function testIzlaz = testF(testUlaz)
+    testIzlaz = testUlaz.^2;
+end
---
+++ ml-and-neural-networks/src/robotika2/testMatlab2Python.m
@@ -0,0 +1,3 @@
+%tester script to verify the idea for a pipeline between matlab and python
+
+izlaz = testF(2);
