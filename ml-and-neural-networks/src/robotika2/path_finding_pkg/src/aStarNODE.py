595c4c72e665fb84401365422f3c2c228cdd0a24
tbiocanin <tekikralj011@gmail.com>
2023-03-30 01:02:21 +0200

    current node structures added, ready for further impl

---
+++ ml-and-neural-networks/src/robotika2/path_finding_pkg/src/aStarNODE.py
@@ -0,0 +1,27 @@
+import rospy 
+# from std_msgs.msg import String,Bool
+# from utils.msg import u_raw
+
+# TODO: integrate the implemented algorithms
+# TODO: test it on the new generated maps
+class aStarNODE():
+
+    def __init__(self) -> None:
+        # definisi neophodne instance klase
+
+        # regulacija da li se node sab-uje ili pablisuje
+        """
+        self.pubAgent = rospy.Publisher("/putanja/", (tip podatka) String)
+        self.signSub = rospy.Subscriber("/putanja/", (tip podatka) Bool, callback=self.callbackFja)
+        """
+        pass
+
+    def metodaNode():
+        pass
+
+    def run():
+        rospy.spin()
+
+    """
+    itd, dodajes sta je neophodno da radi node
+    """
---
+++ ml-and-neural-networks/src/robotika2/sensor_fusion/src/lightSensorNODE.py
@@ -0,0 +1,28 @@
+import rospy 
+# from std_msgs.msg import String,Bool
+# from utils.msg import u_raw
+
+# TODO: use the nn object for detecting relevant objects on the map
+# TODO: test it in a simulated enviroment 
+
+class lightSensorNODE():
+
+    def __init__(self) -> None:
+        # definisi neophodne instance klase
+
+        # regulacija da li se node sab-uje ili pablisuje
+        """
+        self.pubAgent = rospy.Publisher("/putanja/", (tip podatka) String)
+        self.signSub = rospy.Subscriber("/putanja/", (tip podatka) Bool, callback=self.callbackFja)
+        """
+        pass
+
+    def metodaNode():
+        pass
+
+    def run():
+        rospy.spin()
+
+    """
+    itd, dodajes sta je neophodno da radi node
+    """
